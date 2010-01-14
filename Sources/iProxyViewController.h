50b952527ed3d6f5bdab28c3e0357a7a76fe1000
Torsten Curdt <tcurdt@vafer.org>
2010-01-14 02:43:56 +0100

    init

---
+++ Sources/iProxyViewController.h
@@ -0,0 +1,16 @@
+//
+//  iProxyViewController.h
+//  iProxy
+//
+//  Created by Torsten Curdt on 23.12.09.
+//  Copyright vafer.org 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iProxyViewController : UIViewController {
+
+}
+
+@end
+
---
+++ Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iProxy
+//
+//  Created by Torsten Curdt on 23.12.09.
+//  Copyright vafer.org 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
