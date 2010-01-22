7e28e9b3d143fbb99103098fee397365bbdefb23
Jon Maddox <jon@jonsthoughtsoneverything.com>
2010-01-22 16:45:29 -0500

    clean this up

--- InstructionsViewController.h
+++ InstructionsViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface InstructionsViewController : UITableViewController {
+@interface InstructionsViewController : UIViewController {
 }
 
 @end
--- InstructionsViewController.m
+++ InstructionsViewController.m
@@ -17,18 +17,26 @@
 
 - (void)loadView{
   [super loadView];
+  
+  [self setTitle:@"Instructions"];
 
   UIImage *instructionsImage = [UIImage imageNamed:@"instructions.jpg"];
 	UIImageView *imageView = [[[UIImageView alloc] initWithImage:instructionsImage] autorelease];
 
   UIScrollView *scrollView = [[[UIScrollView alloc] initWithFrame:CGRectZero] autorelease];
-
-	[scrollView setFrame:CGRectMake(0, 44, self.view.bounds.size.width, self.view.bounds.size.height - 44.0)];
+  [scrollView setBackgroundColor:[UIColor blackColor]];
+	[scrollView setFrame:self.view.bounds];
 	scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 	scrollView.contentSize = instructionsImage.size;
 
   [scrollView addSubview:imageView];
   [self.view addSubview:scrollView];
+  
+  self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(close)];
+}
+
+- (void)close{
+  [self dismissModalViewControllerAnimated:YES];
 }
 
 /*
@@ -80,84 +88,6 @@
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
 }
-
-
-#pragma mark Table view methods
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-
-// Customize the number of rows in the table view.
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
-}
-
-
-// Customize the appearance of table view cells.
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
-    
-    // Set up the cell...
-	
-    return cell;
-}
-
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Navigation logic may go here. Create and push another view controller.
-	// AnotherViewController *anotherViewController = [[AnotherViewController alloc] initWithNibName:@"AnotherView" bundle:nil];
-	// [self.navigationController pushViewController:anotherViewController];
-	// [anotherViewController release];
-}
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 
 
 @end
